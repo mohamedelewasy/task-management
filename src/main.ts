37c09f322139776734cb9e8e4691336336520c03
mohamedelewasy <eng.m.elewasy@gmail.com>
2023-07-27 15:08:09 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import env from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: env().frontUrl });
   await app.listen(env().port, () =>
     console.log(
       `server runs on port ${env().port} in environment ${env().environment}`,
